bfed5ee1abd9f7e524bac3add5204ce027923234
Vivek Mishra <mishravivek@microsoft.com>
2025-04-15 17:36:30 -0500

    update the chat message

--- src/main/java/com/sk/controller/AIController.java
+++ src/main/java/com/sk/controller/AIController.java
@@ -1,126 +1,243 @@
 package com.sk.controller;
 
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
+import com.microsoft.semantickernel.orchestration.InvocationContext;
+import com.microsoft.semantickernel.orchestration.InvocationReturnMode;
+import com.microsoft.semantickernel.orchestration.ToolCallBehavior;
+import com.microsoft.semantickernel.plugin.KernelPlugin;
+import com.microsoft.semantickernel.plugin.KernelPluginFactory;
 import com.microsoft.semantickernel.services.ServiceNotFoundException;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
+import com.sk.config.AzureAIConfig;
+import com.sk.kernel.kernelUtil;
 import com.sk.model.ChatRequest;
 import com.sk.model.Message;
+import com.sk.plugins.AISearchPlugin;
+import com.sk.plugins.DateTimePlugin;
+import com.sk.plugins.GeocodingPlugin;
+import com.sk.plugins.WeatherPlugin;
 import com.sk.service.AIService;
-import com.sk.model.Response;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class AIController {
 
-	@Autowired
-	AIService aiService;
-	//Create sample RESR API method
-	@GetMapping("/hello")
-	public String getAI() {
-		return "AI";
-	}
+    @Autowired
+    AIService aiService;
 
-	@PostMapping("/chapter2")
-	public ResponseEntity<String> getchapter2(@RequestBody Message message) throws IOException, ServiceNotFoundException {
-		System.out.println("Message: " + message.getContent());
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    AzureAIConfig config;
 
-		return ResponseEntity.ok().headers(headers).body(aiService.extractChapter2Message(message.getContent().trim()));
-	}
+    @Autowired
+    kernelUtil kernelUtil;
 
-	@PostMapping("/reset-session")
-	public String resetSession(HttpSession session) {
-		System.out.println("Session reset-> new session id: " + session.getId());
-		session.invalidate();  // ✅ This will destroy the existing session
-		return "Session reset successfully";
-	}
+    @GetMapping("/hello")
+    public String getAI() {
+        return "AI";
+    }
 
-	@PostMapping("/chapter3")
-	public ResponseEntity<String> getchapter3(@RequestBody Message message) throws IOException, ServiceNotFoundException {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+    @PostMapping("/chapter2")
+    public ResponseEntity<String> getchapter2(@RequestBody Message message) throws IOException, ServiceNotFoundException {
+        System.out.println("Message: " + message.getContent());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-		return ResponseEntity.ok().headers(headers).body(aiService.extractChapter3Message(message.getContent().trim()));
-	}
+        return ResponseEntity.ok().headers(headers).body(aiService.extractChapter2Message(message.getContent().trim()));
+    }
 
-	@PostMapping("/chapter4")
-	public ResponseEntity<String> getchapter4(@RequestBody Message message) throws IOException, ServiceNotFoundException {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+    @PostMapping("/reset-session")
+    public String resetSession(HttpSession session) {
+        System.out.println("Session reset-> new session id: " + session.getId());
+        session.invalidate();  // ✅ This will destroy the existing session
+        return "Session reset successfully";
+    }
 
-		return ResponseEntity.ok().headers(headers).body(aiService.extractChapter4Message(message.getContent().trim()));
-	}
+    @PostMapping("/chapter3")
+    public ResponseEntity<String> getchapter3(@RequestBody Message message) throws IOException, ServiceNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-	@PostMapping("/chapter5")
-	public ResponseEntity<String> getchapter5(@RequestBody Message message) throws IOException, ServiceNotFoundException {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+        return ResponseEntity.ok().headers(headers).body(aiService.extractChapter3Message(message.getContent().trim()));
+    }
 
-		return ResponseEntity.ok().headers(headers).body(aiService.extractChapter5Message(message.getContent().trim()));
-	}
+    @PostMapping("/chapter4")
+    public ResponseEntity<String> getchapter4(@RequestBody Message message) throws IOException, ServiceNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-	@PostMapping("/skChat")
-	public ResponseEntity<List<Response>> getskChat(@RequestBody ChatRequest chatRequest) throws IOException, ServiceNotFoundException {
-		System.out.println("Message: " + chatRequest.getMessages().get(0).getContent());
+        return ResponseEntity.ok().headers(headers).body(aiService.extractChapter4Message(message.getContent().trim()));
+    }
 
-		List<Response> responselist = new ArrayList<>();
-		Response response = new Response();
+    @PostMapping("/chapter5")
+    public ResponseEntity<String> getchapter5(@RequestBody Message message) throws IOException, ServiceNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-		// Setting Role
-		/*Response.Role role = new Response.Role();
-		role.setLabel("Assistant");
-		response.setRole(role);*/
+        return ResponseEntity.ok().headers(headers).body(aiService.extractChapter5Message(message.getContent().trim()));
+    }
 
-		// Setting Items
-		Response.Item item = new Response.Item();
-		//item.set$type("TextContent");
-		item.setText(aiService.chatResponse(chatRequest.getMessages().get(0).getContent()));
-		response.setItems(List.of(item));
+    @PostMapping("/skChat")
+    public ResponseEntity<List<ChatMessageContent<?>>> getskChat(@RequestBody ChatRequest chatRequest) throws IOException, ServiceNotFoundException {
 
-		// Setting ModelId
-		//response.setModelId("gpt-4o");
+        Kernel kernel = kernelBuilder();
+
+        // challange 3, for adding the plugins
+        kernel = AddPlugins(kernel);
 
 
-		// Setting Metadata
 
-		/*Response.Metadata metadata = new Response.Metadata();
-		metadata.setId("chatcmpl-BI4EcluQGKRmOnQTODKmTDVe91gDD");
-		metadata.setCreatedAt("2025-04-03T02:04:42+00:00");
-		metadata.setSystemFingerprint("fp_ded0d14823");
+		/* Challenge 2 for Create chatCompletionService
+		 Initialize a new ChatHistory object to store the conversation history.
+		 Retrieve the ChatCompletionService from the kernel to handle chat completions.
+		 Iterate through the list of user messages from the ChatRequest object
+		 and add each message to the chat history as a user message.
+		*/
+        ChatHistory chatHistory = new ChatHistory();
+        ChatCompletionService chatCompletionService = kernel.getService(ChatCompletionService.class);
+        for (ChatRequest.Message message : chatRequest.getMessages()) {
+            chatHistory.addUserMessage(message.getContent());
+        }
 
-		Response.Usage usage = new Response.Usage();
-		usage.setOutputTokenCount(11);
-		usage.setInputTokenCount(655);
-		usage.setTotalTokenCount(666);
-		usage.setOutputTokenDetails(Map.of(
-			"ReasoningTokenCount", 0,
-			"AudioTokenCount", 0,
-			"AcceptedPredictionTokenCount", 0,
-			"RejectedPredictionTokenCount", 0
-		));
-		usage.setInputTokenDetails(Map.of(
-			"AudioTokenCount", 0,
-			"CachedTokenCount", 0
-		));
-		metadata.setUsage(usage);
+        InvocationContext invocationContext = null;
 
-		metadata.setRefusal(null);
-		metadata.setFinishReason("Stop");
-		metadata.setContentTokenLogProbabilities(List.of());
+		/* Challenge 03 for Create the InvocationContext
+		 Build an InvocationContext object to configure the behavior and return mode
+		 for the invocation of the ChatCompletionService. This configuration allows
+		 all kernel functions to be called and ensures the full conversation history
+		 is returned as part of the response.*/
+        invocationContext = InvocationContext.builder()
+                .withToolCallBehavior(ToolCallBehavior.allowAllKernelFunctions(true)) // Allow unrestricted kernel function calls
+                .withReturnMode(InvocationReturnMode.FULL_HISTORY) // Return the full conversation history
+                .build();
 
-		response.setMetadata(metadata);*/
-		responselist.add(response);
-		System.out.println("Response: " + response.getItems().toString());
-		return ResponseEntity.ok(responselist);
-	}
+		/*
+		 Challenge 2 for make the call to the chatCompletionService
+		 Retrieve the list of chat message contents asynchronously from the ChatCompletionService.
+		 This method takes the chat history, kernel, and invocation context as inputs and blocks
+		 until the response is received. The response contains the AI-generated messages.
+		*/
+        List<ChatMessageContent<?>> responses = chatCompletionService.getChatMessageContentsAsync(
+                chatHistory,
+                kernel,
+                invocationContext
+        ).block();
+
+        if (responses == null || responses.isEmpty()) {
+            throw new ServiceNotFoundException("No response from the service");
+        }
+        // Add AI response to chat history
+        chatHistory.addAll(responses);
+        return ResponseEntity.ok(responses);
+    }
+
+
+	/*
+	 * Adds plugins to the Kernel instance.
+	 *
+	 * This method creates and configures various plugins, including:
+	 * - DateTimePlugin: Provides date and time-related functionalities.
+	 * - GeocodingPlugin: Handles geocoding operations using the provided configuration and RestTemplate.
+	 * - WeatherPlugin: Fetches weather-related data using the provided configuration and RestTemplate.
+	 * - AISearchPlugin: Enables AI-based search functionalities using the provided configuration and kernel utility.
+	 *
+	 * Optionally, a food plugin can be added by uncommenting the relevant code.
+	 *
+	 * The method then builds a Kernel instance with the configured plugins and returns it.
+	 *
+	 * @return A Kernel instance with the configured plugins.
+	 * @throws IOException If an error occurs during plugin creation or kernel building.
+	 */
+    private Kernel AddPlugins(Kernel kernel) throws IOException {
+        // Challenge 03 START for Create the DateTimePlugin
+        KernelPlugin dateTimePlugin = KernelPluginFactory
+                .createFromObject(new DateTimePlugin(), "DateTimePlugin");
+
+        KernelPlugin geoPlugin = KernelPluginFactory
+                .createFromObject(new GeocodingPlugin(config, restTemplate), "GeocodingPlugin");
+
+        KernelPlugin weatherPlugin = KernelPluginFactory
+                .createFromObject(new WeatherPlugin(config, restTemplate), "WeatherPlugin");
+
+        // Challenge 04, Uncomment bellow line for AI search plugin and add the plugin to the list
+        KernelPlugin AISearch = KernelPluginFactory
+                .createFromObject(new AISearchPlugin(config, kernelUtil), "AISearchPlugin");
+
+        // Challenge 05, Uncomment bellow line for food plugin and add the plugin to the list
+        KernelPlugin foodplugin = KernelPluginFactory
+                .importPluginFromDirectory(Path.of("src/main/resources"),
+                        "promptconfig", null);
+
+        kernel = addPluginwithKernel(kernel,List.of(dateTimePlugin, geoPlugin, weatherPlugin, AISearch,foodplugin));
+
+        return kernel;
+    }
+
+    public Kernel addPluginwithKernel(Kernel kernel, List<KernelPlugin> plugins) throws IOException {
+
+        Kernel.Builder kernelBuilder = kernel.toBuilder();
+        if (plugins != null) {
+            for (KernelPlugin plugin : plugins) {
+                kernelBuilder.withPlugin(plugin);
+            }
+        }
+        return kernelBuilder.build();
+
+
+    }
+
+    public Kernel kernelBuilder() throws IOException {
+
+        // Challenge 2 for Create the client
+		/*
+		 Create an OpenAIAsyncClient instance using the OpenAIClientBuilder.
+		 This client is configured with the API key and endpoint from the AzureAIConfig.
+		 It is used to interact with OpenAI services asynchronously.
+		*/
+        OpenAIAsyncClient clientasync = new OpenAIClientBuilder()
+                .credential(new AzureKeyCredential(config.getOpenAiApiKey()))
+                .endpoint(config.getOpenAiEndpoint())
+                .buildAsyncClient();
+
+        // Challenge 2 Create the chat completion service
+		/*
+		 Create a ChatCompletionService instance using the OpenAIChatCompletion builder.
+		 This service is configured with the OpenAIAsyncClient and the model ID from the AzureAIConfig.
+		 It is used to handle chat completions.
+		*/
+        ChatCompletionService chatCompletion = OpenAIChatCompletion.builder()
+                .withOpenAIAsyncClient(clientasync)
+                .withModelId(config.getChatDeploymentName())
+                .build();
+
+        Kernel kernel = null;
+		/*
+		 Build a Kernel instance using the Kernel builder.
+		 Configure it with the ChatCompletionService and finalize the build process.
+		*/
+        kernel = Kernel.builder()
+                .withAIService(ChatCompletionService.class, chatCompletion)
+                .build();
+
+        return kernel;
+
+    }
 }
